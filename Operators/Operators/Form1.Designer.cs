860d372324154fbfe3276ca9e5988aefd54a08a3
Syomai <126589688+Syomaii@users.noreply.github.com>
2023-05-20 17:49:36 +0800

    Add files via upload

---
+++ Operators/Operators/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Operators
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxValue = new System.Windows.Forms.TextBox();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxValue
+            // 
+            this.txtBoxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxValue.Location = new System.Drawing.Point(215, 125);
+            this.txtBoxValue.Name = "txtBoxValue";
+            this.txtBoxValue.Size = new System.Drawing.Size(199, 34);
+            this.txtBoxValue.TabIndex = 0;
+            this.txtBoxValue.TextChanged += new System.EventHandler(this.txtBoxValue_TextChanged);
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValue.Location = new System.Drawing.Point(116, 128);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(93, 29);
+            this.lblValue.TabIndex = 1;
+            this.lblValue.Text = "Value: ";
+            this.lblValue.Click += new System.EventHandler(this.lblValue_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(138, 199);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(140, 34);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(220, 263);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 29);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(149, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Accumulator Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(113, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Result: ";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(337, 199);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(140, 34);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(577, 338);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.txtBoxValue);
+            this.Name = "Form1";
+            this.Text = "Accumulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxValue;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Operators/Operators/Form1.cs
@@ -0,0 +1,115 @@
+﻿/*
+    Name: Christian Jay Putol
+    Date Created: 3/19/2023
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Operators
+{
+    public partial class Form1 : Form
+    {
+        double accumulator = 0.0;
+        int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            lblResult.Visible = true;
+
+            string input = txtBoxValue.Text.Trim();
+            char op;
+            double operand;
+            ScanData(input, out op, out operand);
+            DoNextOperation(op, operand, ref accumulator);
+
+            counter++;
+            if (op != 'q' && counter <= 1)
+            {
+                lblResult.Text = $"result so far is {accumulator:N1}";
+            }
+            else if (op == 'q' || op == 'Q')
+            {
+                lblResult.Text = $"Final result is {accumulator:N1}";
+                txtBoxValue.Enabled = false;
+                btnCalculate.Enabled = false;
+            }
+            else if (op != 'q' && counter > 1)
+            {
+                lblResult.Text = $"the result so far is {accumulator:N1}";
+            }
+
+        }
+
+        private void ScanData(string input, out char op, out double operand)
+        {
+            op = ' ';
+            operand = 0;
+            try
+            {
+                string[] tokens = input.Split(' ');
+                op = char.Parse(tokens[0]);
+                operand = double.Parse(tokens[1]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    
+
+        private void DoNextOperation(char op, double operand, ref double accumulator)
+        {
+            switch (op)
+            { 
+                case '+': accumulator += operand; break;
+                case '-': accumulator -= operand; break;
+                case '*': accumulator *= operand; break;
+                case '/': accumulator /= operand; break;
+                case '^': accumulator = Math.Pow(accumulator, operand); break;
+                case 'q': case 'Q':  break;
+                default:
+                    counter = 0;
+                    MessageBox.Show("The valid operators are \n + add \n - minus \n * multiply \n / division \n ^ root \n q for quit","Invalid",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    lblResult.Text = "Error";
+                    lblResult.Visible = false;
+                    txtBoxValue.Enabled = true;
+                    btnCalculate.Enabled = true;
+                break;
+            }
+            txtBoxValue.Clear();
+            
+        }
+
+        private void txtBoxValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblValue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            accumulator = 0.0;
+            lblResult.Text = "0";
+            lblResult.Visible = false;
+            txtBoxValue.Enabled = true;
+            btnCalculate.Enabled = true;
+        }
+    }
+}
